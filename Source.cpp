e169127714ea2f74ab9cbe89ebcdf76338d4a73a
kenjinote <2605401+kenjinote@users.noreply.github.com>
2023-05-15 11:00:26 +0900

    プロジェクト ファイルを追加します。

---
+++ Source.cpp
@@ -0,0 +1,7 @@
+﻿#include <windows.h>
+
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
+{
+	SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, (LPARAM)2);
+	return 0;
+}
